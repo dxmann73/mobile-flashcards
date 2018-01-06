700835987bd21b16747cf4ee9cb09a3b72b3ecea
dave <dxmann73@gmail.com>
2018-01-06 19:44:02 +0000

    feat: Avoid keyboard when adding decks; don't submit empty titles

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import reducer from './reducers';
-import {View} from 'react-native';
+import {KeyboardAvoidingView, View} from 'react-native';
 import FcStatusBar from './components/shared/FcStatusBar';
 import {defaultStyles} from './styles/default';
 import {appNavigationBackgroundColor} from './styles/colors';
--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {KeyboardAvoidingView, StyleSheet, Text, TextInput} from 'react-native';
 import FcButton from './shared/FcButton';
 import {defaultStyles} from '../styles/default';
 import {addDeck} from '../actions/index';
@@ -9,23 +9,25 @@ import {appLightInk} from '../styles/colors';
 class AddDeck extends React.Component {
     state = {text: ''};
     addDeck = (title) => {
-        this.props.dispatchAddDeck(title);
-        this.props.navigation.navigate('AddCard', {title});
+        if (title.length > 0) {
+            this.props.dispatchAddDeck(title);
+            this.props.navigation.navigate('AddCard', {title});
+        }
         this.setState({text: ''});
     };
 
     render() {
-        return <View style={defaultStyles.mainView}>
+        return <KeyboardAvoidingView behavior={'padding'} keyboardVerticalOffset={65} style={defaultStyles.mainView}>
             <Text style={styles.heading}>What is the title</Text>
             <Text style={styles.heading}>of your new</Text>
             <Text style={styles.heading}>deck?</Text>
             <TextInput style={defaultStyles.inputField}
-                       placeholder="Deck title"
+                       placeholder="Please add a deck title"
                        onChangeText={(text) => this.setState({text})}
                        value={this.state.text}
             />
-            <FcButton onPress={() => this.addDeck(this.state.text)} buttonText={'Submit'} inverted={true} />
-        </View>;
+            <FcButton onPress={() => this.addDeck(this.state.text.trim())} buttonText={'Submit'} inverted={true} />
+        </KeyboardAvoidingView>;
     };
 }
 
